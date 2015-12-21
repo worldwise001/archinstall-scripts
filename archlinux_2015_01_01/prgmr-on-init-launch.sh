53b18bccd0854db6f1ce3ce315baf892cd08888f
Ben Ennis <ben@outernet.is>
2015-12-20 20:47:54 -0800

    removed obsolete version - images no longer exist

--- archlinux_2015_01_01/prgmr-on-init-launch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo 'launch script execution occurred'
-haveged -w 1024 -v 1
-pacman-key --init
-pkill haveged
-pacman-key --populate archlinux
-
-systemctl disable prgmr-on-init-launch
