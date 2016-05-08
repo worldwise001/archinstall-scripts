c24e0e7feca05daea432b32b4d84bf5b0fd6cff5
Sarah Harvey <sharvey@metaether.net>
2016-05-07 17:13:45 -0700

    Add site-specific bootstrap bits

--- archlinux_2016_05_01/archinstall.sh
+++ archlinux_2016_05_01/archinstall.sh
@@ -2,37 +2,35 @@
 # from live rescue, single user mode...
 
 # Partition the disk
-echo -e 'o\nn\np\n1\n\n\nw' | fdisk -u /dev/xvda
-mkfs.ext4 /dev/xvda1
+echo -e 'o\nn\np\n1\n\n+10G\nn\np\n2\n\n+512M\nt\n2\n82\nn\np\n3\n\n\nw' | fdisk -u /dev/xvda
+yes | mkfs.ext4 /dev/xvda1
+mkswap /dev/xvda2
+yes | mkfs.ext4 /dev/xvda3
 mount /dev/xvda1 /mnt
 
+# Download and Extract tar
+mkdir -p /mnt/opt/install
+pushd /mnt/opt/install
+wget https://mirrors.kernel.org/archlinux/iso/2016.05.01/archlinux-bootstrap-2016.05.01-x86_64.tar.gz --output-document=arch-2016.05.01-64.tar.gz
+echo "Extracting bootstrap"
+tar -zxf arch-2016.05.01-64.tar.gz
+
 # Copy scripts into place
-mkdir /mnt/opt/
-mkdir /mnt/opt/install
-mkdir  /mnt/opt/install/root.x86_64
-mkdir  /mnt/opt/install/root.x86_64/opt/
+popd
 cp archinstall_archboot /mnt/opt/install/root.x86_64/opt/
 cp archinstall_finalize /mnt/opt/install
 cp prgmr-on-init-launch.s* /mnt/opt/install
 
-# Download and Extract tar
-cd /mnt/opt/install
-wget https://mirrors.kernel.org/archlinux/iso/2016.05.01/archlinux-bootstrap-2016.05.01-x86_64.tar.gz --output-document=arch-2016.05.01-64.tar.gz
-tar -zxvf arch-2016.05.01-64.tar.gz
-
 # chroot into arch environment
 cp /etc/resolv.conf /mnt/opt/install/root.x86_64/etc
-mount --rbind /proc /mnt/opt/install/root.x86_64/proc
-mount --rbind /sys /mnt/opt/install/root.x86_64/sys
-mount --rbind /dev /mnt/opt/install/root.x86_64/dev
-mount --rbind /run /mnt/opt/install/root.x86_64/run
+for i in {proc,sys,dev,run}; do
+  mount --rbind /$i /mnt/opt/install/root.x86_64/$i
+done
 chroot /mnt/opt/install/root.x86_64/ /opt/archinstall_archboot
 
-mount --rbind /proc /mnt/proc
-mount --rbind /sys /mnt/sys
-mount --rbind /dev /mnt/dev
-mount --rbind /run /mnt/run
-
+for i in {proc,sys,dev,run}; do
+  mount --rbind /$i /mnt/$i
+done
 chroot /mnt/ /opt/install/archinstall_finalize
 
 echo "~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~"
